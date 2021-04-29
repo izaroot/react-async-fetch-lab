90deabff54ca0881fd9246265dd19ae2b4814764
Isaac <izaroot327@gmail.com>
2021-04-29 17:54:16 -0400

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component{
+
+    state={
+        message: "",
+        number: 0,
+        people: []
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(astroObj => {
+            this.setState(astroObj)
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                { this.state.people.map(person => <div key={person.name}>Astro: {person.name} || Station: {person.craft} </div> ) }
+            </div>
+        )
+    }
+}
